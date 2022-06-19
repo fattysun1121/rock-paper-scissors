4348e4a26982b426b978d42f0f0bba9e70d4e551
fattysun1121 <85023400+fattysun1121@users.noreply.github.com>
2022-06-19 22:01:47 +0800

    Complete function playRound and make sure it works

--- script.js
+++ script.js
@@ -21,12 +21,30 @@ console.log(computerPlay());
 console.log(computerPlay());
 
 /**
- * Starts one round of rock paper scissors.
+ * Starts one round of P v C rock paper scissors and returns a string that
+ * states the winner.
  */
 function playRound(playerSelection, computerSelection) {
+    // Modifies playerSelection so it becomes case insensitive.
+    playerSelection = playerSelection.toLowerCase();
+    playerSelection = playerSelection.replace(playerSelection.charAt(0), playerSelection.charAt(0).toUpperCase());
 
+    if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "You Win! Scissors beats Paper";
+    } else if (playerSelection === computerSelection) {
+        return `You picked ${playerSelection} and the computer picked ${computerSelection}. It's a tie!`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
 }
 
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
 /**
  * Starts a 5-round game that keeps score and records the winner or loser
  * at the end.
