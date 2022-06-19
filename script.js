f7e625530d9c5132e612b81527e2750f342add40
fattysun1121 <85023400+fattysun1121@users.noreply.github.com>
2022-06-19 21:41:34 +0800

    Lay down skeletons of functions required for rock paper scissors

---
+++ script.js
@@ -0,0 +1,21 @@
+/**
+ * Returns "Rock", "Paper", or "Scissors" randomly.
+ */
+function computerPlay() {
+
+}
+
+/**
+ * Starts one round of rock paper scissors.
+ */
+function playRound(playerSelection, computerSelection) {
+
+}
+
+/**
+ * Starts a 5-round game that keeps score and records the winner or loser
+ * at the end.
+ */
+function game() {
+
+}
