0d0843de274fea41bb46505c18df65fabd4a82cc
fattysun1121 <85023400+fattysun1121@users.noreply.github.com>
2022-06-19 21:51:18 +0800

    Complete function computerPlay and make sure it works

--- script.js
+++ script.js
@@ -2,8 +2,23 @@
  * Returns "Rock", "Paper", or "Scissors" randomly.
  */
 function computerPlay() {
+    // Generates either 0, 1, or 2.
+    let computerChoice = Math.floor(Math.random() * 3);
 
+    // Translates the choice to rock, paper, or scissors.
+    switch (computerChoice) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+    }
 }
+
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
 
 /**
  * Starts one round of rock paper scissors.
