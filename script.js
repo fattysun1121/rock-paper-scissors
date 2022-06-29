d9549cf9b36b1e96fcaf3a70516169ea53962f1b
fattysun1121 <85023400+fattysun1121@users.noreply.github.com>
2022-06-29 21:54:49 +0800

    Finish styling and add a reset button when the game is over

--- script.js
+++ script.js
@@ -49,23 +49,37 @@ function playRound(playerSelection, computerSelection) {
         letter.classList.add('letter-shown');
     }
 
-    recordScore(div);
+    checkScore(div);
 }
 
 /**
- * Starts a 5-round game that keeps score and records the winner or loser
- * at the end.
+ * Deactivate the rock, paper, and scissors buttons.
  */
-function game() {
+ function deactivateButtons() {
     const rock = document.querySelector('.rock');
     const paper = document.querySelector('.paper');
     const scissors = document.querySelector('.scissors');
 
-    rock.addEventListener('click', () => playRound('rock', computerPlay()));
-    paper.addEventListener('click', () => playRound('paper', computerPlay()));
-    scissors.addEventListener('click', () => playRound('scissors', computerPlay()));
+    rock.onclick = null;
+    paper.onclick = null;
+    scissors.onclick = null;
+ }
+/**
+ * Activate the rock, paper, and scissors buttons.
+ */
+function activateButtons() {
+    const rock = document.querySelector('.rock');
+    const paper = document.querySelector('.paper');
+    const scissors = document.querySelector('.scissors');
+
+    rock.onclick = () => playRound('rock', computerPlay());
+    paper.onclick = () => playRound('paper', computerPlay());
+    scissors.onclick = () => playRound('scissors', computerPlay());
 }
 
+/**
+ * Helper function for hiding every letter to reset the game.
+ */
 function hideLetters() {
     for (const char of breakArr) {
         const playerLetter = document.querySelector(`.you-break-block > .${char}`);
@@ -76,31 +90,39 @@ function hideLetters() {
 
 }
 /**
- * Helper function to record the score and declare winner of a 5-point game.
+ * Helper function to check the score and declare winner of a 5-point game.
  */
-function recordScore(board) {
+function checkScore(board) {
     const gameText = document.createElement('p');
+    let keepPlaying = true;
     if (playerScore === 5) {
         gameText.textContent = 'The player is the winner of the game.';
-        hideLetters();
-        playerScore = 0;
-        computerScore = 0;
+        keepPlaying = false;
     } else if (computerScore === 5) {
         gameText.textContent = 'The computer is the winner of the game.';
-        hideLetters();
-        playerScore = 0;
-        computerScore = 0;
-    } else {
-        gameText.textContent = `Player Score: ${playerScore} Computer Score: ${computerScore}`;
+        keepPlaying = false;
     }
-    board.appendChild(gameText);
+    if (!keepPlaying) {
+        board.appendChild(gameText);
+        const resetButton = document.createElement('button');
+        resetButton.textContent = 'Reset!';
+        resetButton.addEventListener('click', reset);
+        board.appendChild(resetButton);
+        deactivateButtons();
+    }
+
 }
 
+function reset() {
+    hideLetters();
+    playerScore = 0;
+    computerScore = 0;
+    activateButtons();
+    const result = document.querySelector('.result');
+    result.innerHTML = '';
+}
 
 // set up webpage
-
-
-
 const loaderPara = document.querySelector('.loader > p');
 const loaderText = `Break The Game is a breakdance game similar to the game HORSE in
        basketball. This game is usually played by two players, each of
@@ -112,6 +134,9 @@ const speed = 50;
 let i = 0;
 let breakArr = ['B', 'R', 'E', 'A', 'K'];
 
+/**
+ * Shows text one by one on the screen like a typewriter.
+ */
 function typeWriter() {
     if (i < loaderText.length) {
         loaderPara.innerHTML += loaderText.charAt(i);
@@ -119,11 +144,18 @@ function typeWriter() {
         setTimeout(typeWriter, speed);
     }
 }
+
+/**
+ * Hides the loader after it's loaded.
+ */
 function showLoader() {
     const loader = document.querySelector('.loader');
     loader.classList.add('hidden');
 }
 
+/**
+ * Sets up five boxes for scoring.
+ */
 function setUpBreak() {
     const you = document.querySelector('.you > .you-break-block');
     const machine = document.querySelector('.machine > .machine-break-block');
@@ -147,12 +179,11 @@ window.addEventListener('load', () => {
     setUpBreak();
 });
 
-
 // actual game
 let playerScore = 0;
 let computerScore = 0;
 
-game();
+activateButtons();
 
 
 
